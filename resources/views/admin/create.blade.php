2f4544685de6e944afadf2fe3a518b97484bf591
Zzul06 <nurzaky2006@gmail.com>
2024-02-23 09:42:26 +0700

    edit create  style
    belum responsif sm animasi

--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -1,20 +1,65 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('css/styleApp.css') }}" rel="stylesheet">
 
-@section('content')
-    <h1>Create a New Product</h1>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <form action="{{ route('produk.store') }}" method="POST" enctype="multipart/form-data">
-        @csrf
+    @vite('resources/css/app.css')
+    <title>Add Product</title>
+</head>
+    <body>
+        <main>
+            <div class="container-w-screen h-screen">
+            <div class="flex justify-center">
+                <div class=" w-[450px] h-screen p-2">  
+                    <h1 class="text-creedt text-center mb-2 h-16">Add New Product</h1>
 
-        <label for="nama">Product Name:</label>
-        <input type="text" name="nama" id="nama" required>
+                    <form class="w-[450px] h-fit" action="{{ route('produk.store') }}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        <div class="flex-col text-uspas">
+                            <label for="nama">Product Name</label>
+                            
+                            <div class="flex-col mb-1">
+                                <input  type="text" name="nama" id="nama" class="border-2 border-[#FFC93E] rounded-md w-[450px] h-[30px] p-4" value="{{ @old('nama') }}" required autocomplete="off">
+                            </div>
+                        </div>
 
-        <label for="gambar">Product Image:</label>
-        <input type="file" accept="image/*" name="gambar" id="gambar" required>
+                        <div class="flex-col text-uspas">
+                            <label for="link">Product Link</label>
 
-        <label for="link">Product Link:</label>
-        <input type="text" name="link" id="link" required>
+                            <div class="flex-col mb-1">
+                                <input  type="text" name="link" id="link" class="border-2 border-[#FFC93E] rounded-md w-[450px] h-[30px] p-4" value="{{ @old('link') }}" required autocomplete="off">
+                            </div>
+                        </div>
 
-        <button type="submit">Create Product</button>
-    </form>
-@endsection
+                        <div class="flex-col text-uspas">
+                            <label for="gambar">Product Image</label>
+
+                            <div class="flex mb-3">
+                                <input type="file" accept="image/*" name="gambar" id="gambar" class=" border-2 border-[#FFC93E] rounded-md w-[450px] h-[250px] p-4 
+                                                                                                        file:border-2 file:border-[#FFC93E] file:rounded-[12px]
+                                                                                                        file:mx-auto file:my-20 file:w-[150px] file:ml-8 file:mr-8 file:bg-white " required>
+                            </div>
+                        </div>
+
+                        <div class="flex justify-stretch gap-8 ">
+                                <a href="/" class="butlog flex-col text-butlog  bg-[#D9D9D9] rounded-[12px] w-[210px] h-[40px] text-center p-[5px]">Cancel</a>
+                            <div class="flex text-butlog ">
+                                <button type="submit" class="butlog bg-[#D9D9D9] rounded-[12px] w-[210px] h-[40px]">Confirm</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        </main>
+    </body>
+</html>
+
+
+
--- resources/views/admin/edit.blade.php
+++ resources/views/admin/edit.blade.php
@@ -1,30 +1,68 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('css/styleApp.css') }}" rel="stylesheet">
 
-@section('content')
-    <h1>Edit Produk</h1>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <form method="POST" action="{{ route('produk.update', $produk->id) }}" enctype="multipart/form-data">
-        @csrf
-        @method('PUT')
+    @vite('resources/css/app.css')
+    <title>Edit Product</title>
+</head>
+    <body>
+        <main>
+            <div class="container-w-screen h-screen">
+                <div class="flex justify-center">
+                    <div class=" w-[450px] h-screen p-2">
+                    <h1 class="text-creedt text-center mb-2 h-16">Edit Product</h1>
+                    <form class="w-[450px] h-fit" method="POST" action="{{ route('produk.update', $produk->id) }}" enctype="multipart/form-data">
+                        @csrf
+                        @method('PUT')
 
-        <div class="form-group">
-            <label for="nama">Nama</label>
-            <input type="text" name="nama" id="nama" class="form-control" value="{{ old('nama', $produk->nama) }}" required>
-        </div>
+                        <div class="flex-col text-uspas">
+                            <label for="nama">Product Name</label>
 
-        <div class="form-group">
-            <label for="link">Link</label>
-            <input type="text" name="link" id="link" class="form-control" value="{{ old('link', $produk->link) }}" required>
-        </div>
+                            <div class="flex-col mb-1">                           
+                                <input type="text" name="nama" id="nama" class="form-control" value="{{ old('nama', $produk->nama) }}" required>
+                            </div>
+                        </div>    
 
-        <div class="form-group">
-            <label for="gambar">Gambar</label>
-            <input type="file" name="gambar" id="gambar" class="form-control" accept="image/*">
-            @if ($produk->gambar)
-                <img src="{{ Storage::url($produk->gambar) }}" alt="{{ $produk->nama }}" class="img-thumbnail mt-2" width="100">
-            @endif
-        </div>
+                        <div class="flex-col text-uspas">
+                            <label for="link">Product Link</label>
 
-        <button type="submit" class="btn btn-primary">Update</button>
-    </form>
-@endsection
+                            <div class="flex-col mb-1"> 
+                                <input type="text" name="link" id="link" class="form-control" value="{{ old('link', $produk->link) }}" required>
+                            </div>
+                        </div>    
+
+                        <div class="flex-col text-uspas">
+                            <label for="gambar">Product Image</label>
+                                
+                            <div class="flex mb-3">
+                                <input type="file" name="gambar" id="gambar"  accept="image/*" class=" border-2 border-[#FFC93E] rounded-md w-[450px] h-[250px] p-4 
+                                                                                                file:border-2 file:border-[#FFC93E] file:rounded-[12px]
+                                                                                                file:mx-auto file:my-20 file:w-[150px] file:ml-8 file:mr-8 file:bg-white " >
+                                @if ($produk->gambar)
+                                    <img src="{{ Storage::url($produk->gambar) }}" alt="{{ $produk->nama }}" class="img-thumbnail mt-2" width="100">
+                                @endif
+                            </div>
+                        </div>    
+
+                        <div class="flex justify-stretch gap-8 ">
+                            <a href="/" class="butlog flex-col text-butlog  bg-[#D9D9D9] rounded-[12px] w-[210px] h-[40px] text-center p-[5px]">Cancel</a>
+                        <div class="flex text-butlog ">
+                            <button type="submit" class="butlog bg-[#D9D9D9] rounded-[12px] w-[210px] h-[40px]">Update</button>
+                        </div>
+                    </div>
+                    </form>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </body>
+</html>
+
+
